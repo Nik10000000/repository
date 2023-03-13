68bcb7cf21f4e701e85ba29f7bf502e2bd7cdeaf
Nik10000000 <niksalskij@gmail.com>
2023-03-13 16:37:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp30
+{
+    public class Country
+    {
+        public Country()
+        {
+            Console.Write("Введите страну: ");
+            string title = Convert.ToString(Console.ReadLine());
+            Console.Write("Введите форму правления: ");
+            string form_pravlenia = Convert.ToString(Console.ReadLine());
+            float square = 120;
+            Console.WriteLine(title);
+            Console.WriteLine(form_pravlenia);
+            Console.WriteLine("Площадь: "+square);
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country = new Country();
+            Console.WriteLine();
+            Console.ReadKey();
+        
+        }
+    }
+}
